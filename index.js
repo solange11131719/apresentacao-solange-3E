d501cfbf1a56775ccc0633dc3a05120ed2f47ffc
solange11131719 <167382822+solange11131719@users.noreply.github.com>
2024-09-05 17:54:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const primeiroNome = "solange";
+const sobreNome = "generoso";
+const idade = 48;
+const cidadenatal = "mambore";
+const altura = 1.49;
+
+console.log("o meu primeiro nome é")
+console.log(primeiroNome);
+console.log("meu sobrenome é");
+console.log(sobreNome);
+console.log("minha idade é");
+console.log(idade);
+console.log("minha cidade é");
+console.log(cidadenatal);
